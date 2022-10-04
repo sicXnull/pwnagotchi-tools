e14d404ae6529970c16102ec19c4963e663e73dc
sicxnull <cvanhauterjr@gmail.com>
2022-10-03 18:34:03 -0700

    python/pip

--- linux.sh
+++ linux.sh
@@ -13,7 +13,9 @@ scp -r ~/.ssh/id_rsa.pub root@10.0.0.2:/home/pi/handshakes/*.pcap $INSTALL_LOCAT
 scp -r ~/.ssh/id_rsa.pub root@10.0.0.2:/root/handshakes/*.pcap $INSTALL_LOCATION/pwnagotchi-tools/handshakes/pcap
 rm $INSTALL_LOCATION/pwnagotchi-tools/handshakes/pcap/*.pub
 
-apt-get -y --allow-unauthenticated update && apt-get install -y curl
+apt-get -y --allow-unauthenticated update \
+&& apt-get install -y curl python3-pip
+pip install wordninja tabulate
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
 wget http://old-releases.ubuntu.com/ubuntu/pool/universe/h/hcxtools/hcxtools_6.0.2-1_amd64.deb
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - && apt-get -y install nodejs
@@ -25,3 +27,4 @@ npm install
 npm install dotenv
 node pwnagetty.js
 chmod -R a+rwx $INSTALL_LOCATION/pwnagotchi-tools/handshakes/*
+
