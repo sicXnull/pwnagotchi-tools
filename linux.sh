2ea25feb01862bb330001cdf4c2023ab85da97a8
sicxnull <cvanhauterjr@gmail.com>
2022-10-03 17:22:52 -0700

    no need for 2 scripts

--- linux.sh
+++ linux.sh
@@ -4,6 +4,13 @@ INSTALL_LOCATION=/home/sic/Desktop
 
 echo "*** MUST BE RUN AS ROOT ***"
 
+mkdir $INSTALL_LOCATION/pwnagotchi-tools/handshakes/pcap
+ssh-keygen -t rsa
+ssh-copy-id root@10.0.0.2
+scp -r ~/.ssh/id_rsa.pub root@10.0.0.2:/home/pi/handshakes/*.pcap $INSTALL_LOCATION/pwnagotchi-tools/handshakes/pcap
+scp -r ~/.ssh/id_rsa.pub root@10.0.0.2:/root/handshakes/*.pcap $INSTALL_LOCATION/pwnagotchi-tools/handshakes/pcap
+rm $INSTALL_LOCATION/pwnagotchi-tools/handshakes/pcap/*.pub
+
 apt-get -y --allow-unauthenticated update  && apt-get install -y curl
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
 wget http://old-releases.ubuntu.com/ubuntu/pool/universe/h/hcxtools/hcxtools_6.0.2-1_amd64.deb
