2de9d86d19af33d9bd568b7af69fd7706b3ef35f
sicxnull <cvanhauterjr@gmail.com>
2022-10-03 17:13:39 -0700

    fix

--- linux-get-pcap.sh
+++ linux-get-pcap.sh
@@ -2,6 +2,7 @@
 # Author: sic
 INSTALL_LOCATION=~/Desktop
 
+mkdir $INSTALL_LOCATION/pwnagotchi-tools/handshakes/pcap
 ssh-keygen -t rsa
 ssh-copy-id root@10.0.0.2
 scp -r ~/.ssh/id_rsa.pub root@10.0.0.2:/home/pi/handshakes/*.pcap $INSTALL_LOCATION/pwnagotchi-tools/handshakes/pcap
