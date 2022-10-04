c05e48b4381e03b8b610aaaf7832e00d49e2c063
sicxnull <cvanhauterjr@gmail.com>
2022-10-03 17:05:05 -0700

    clean up

--- pwnagetty.sh
+++ pwnagetty.sh
@@ -4,14 +4,12 @@ INSTALL_LOCATION=/home/sic/Desktop
 
 echo "*** MUST BE RUN AS ROOT ***"
 
-apt-get -y --allow-unauthenticated update
-apt-get -y install curl
+apt-get -y --allow-unauthenticated update  && apt-get install -y curl
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
-apt-get -y install ./libssl1.1_1.1.1f-1ubuntu2_amd64.deb
 wget http://old-releases.ubuntu.com/ubuntu/pool/universe/h/hcxtools/hcxtools_6.0.2-1_amd64.deb
-apt-get -y install ./hcxtools_6.0.2-1_amd64.deb
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-apt-get -y install nodejs
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - && apt-get -y install nodejs
+apt-get install -y ./libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+apt-get install -y ./hcxtools_6.0.2-1_amd64.deb
 rm libssl1.1_1.1.1f-1ubuntu2_amd64.deb hcxtools_6.0.2-1_amd64.deb
 cd $INSTALL_LOCATION/pwnagotchi-tools/handshakes/pwnagetty
 npm install
